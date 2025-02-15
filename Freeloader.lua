1e80665e40641469651b8593cf15d5cadbcefcca
Eta <pleasefckoff00@gmail.com>
2025-02-15 19:04:30 +0000

    Update Freeloader.lua

--- Freeloader.lua
+++ Freeloader.lua
@@ -1,5 +1,3 @@
-shared.UseWebhook = false
-shared.Webhook = ""
 
 if game.PlaceId == 16732694052 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ProjektEta/Femscr.dll/refs/heads/main/Scripts/Fisch.lua"))()
